d1c9c77929c81caf315b2ee8c75a183119845cb6
Hiruni <s16921@sci.pdn.ac.lk>
2020-06-12 12:59:30 +0530

    checkCoding

---
+++ main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+
+class MyAppState extends State<MyApp>{
+  var questionIndex=0;
+
+  void answerQuetion(){
+    setState(() {
+      questionIndex=questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favourite color?',
+      'what\'s your favourite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuetion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed:() => print('Answer 2 choosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: (){
+                print('answer 3 choosen!');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
